031c8b00af001f56d7cfcbc0a96c9efb7c21ab11
pawel <pawelzell@gmail.com>
2019-12-05 11:47:26 +0100

    update scripts

--- deploy/metrics-up.sh
+++ deploy/metrics-up.sh
@@ -1,8 +1,8 @@
 # TODO you need to install helm
 helm init &&
   kubectl create serviceaccount --namespace kube-system tiller &&
-	kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=kube-system:tiller &&
-	kubectl patch deploy --namespace kube-system tiller-deploy -p '{"spec":{"template":{"spec":{"serviceAccount":"tiller"}}}}' &&
+  kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=kube-system:tiller &&
+  kubectl patch deploy --namespace kube-system tiller-deploy -p '{"spec":{"template":{"spec":{"serviceAccount":"tiller"}}}}' &&
   kubectl apply -f crb-kubelet-api-admin.yaml &&
   until helm install --name metrics-server stable/metrics-server --namespace metrics --set args={"--kubelet-insecure-tls=true,--kubelet-preferred-address-types=InternalIP\,Hostname\,ExternalIP"}
 	do
--- deploy/up.sh
+++ deploy/up.sh
@@ -1,5 +1,4 @@
-# TODO list files in serviceAccounts and create each of them
-# TODO add info that KUBECONFIG needs to be set to use kubectl
+# Script for kubernetes v1.15
 kubectl apply -f serviceAccounts/default\:type-aware-scheduler &&
   kubectl apply -f serviceAccounts/kube-system\:type-aware-scheduler &&
   kubectl create clusterrolebinding type-aware-scheduler-admin --clusterrole=cluster-admin --serviceaccount=default:type-aware-scheduler &&
@@ -8,11 +7,11 @@ kubectl apply -f serviceAccounts/default\:type-aware-scheduler &&
   # Setup grafana and influxdb:
   kubectl create secret generic influxdb-creds  --from-literal=INFLUXDB_DATABASE=type_aware_scheduler --from-literal=INFLUXDB_USERNAME=root --from-literal=INFLUXDB_PASSWORD=root --from-literal=INFLUXDB_HOST=influxdb &&
   kubectl create -f influxdb-pvc.yaml &&
-  kubectl apply -f influxdb.yaml &&
+  kubectl apply -f influxdb-1-15.yaml &&
   kubectl expose deployment influxdb --port=8086 --target-port=8086 --protocol=TCP --type=ClusterIP &&
   kubectl create secret generic grafana-creds --from-literal=GF_SECURITY_ADMIN_USER=admin --from-literal=GF_SECURITY_ADMIN_PASSWORD=graphsRcool &&
   kubectl create configmap grafana-config --from-file=influxdb-datasource.yml=influxdb-datasource.yml --from-file=grafana-dashboard-provider.yml=grafana-dashboard-provider.yml &&
-  kubectl apply -f grafana.yaml &&
+  kubectl apply -f grafana-1-15.yaml &&
   kubectl expose deployment grafana --type=NodePort --port=3000 # &&
 #kubectl expose deployment grafana --type=LoadBalancer --port=80 --target-port=3000 --protocol=TCP
 # TODO influxdb and grafana manage password
