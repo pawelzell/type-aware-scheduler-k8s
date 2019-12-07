e7d4f4147c1173ace380017c865ca504436397dd
pawel <pawelzell@gmail.com>
2019-12-07 08:01:52 +0100

    kub_post_init.sh script

---
+++ deploy/kub_post_init.sh
@@ -0,0 +1,4 @@
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
