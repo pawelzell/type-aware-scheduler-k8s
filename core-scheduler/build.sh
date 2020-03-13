9b9d2711602f2cac2cfb12352bb5e29d6211d0c0
pawel <pawelzell@gmail.com>
2020-03-13 15:27:08 +0100

    Fix container name

--- core-scheduler/build.sh
+++ core-scheduler/build.sh
@@ -1,5 +1,5 @@
 go build -o app . &&
-  docker build . --tag type-aware-scheduler || exit 1
+  docker build . --tag pawelzell/type-aware-scheduler || exit 1
 
 docker push pawelzell/type-aware-scheduler
 kind load docker-image type-aware-scheduler
