36863a8fe093c7a16ef3308b52b77491faae8701
pawel <pawelzell@gmail.com>
2020-03-13 14:20:28 +0100

    pull policy always

--- core-scheduler/build.sh
+++ core-scheduler/build.sh
@@ -1,3 +1,5 @@
 go build -o app . &&
-  docker build . --tag type-aware-scheduler &&
-  kind load docker-image type-aware-scheduler
+  docker build . --tag type-aware-scheduler || exit 1
+
+docker push pawelzell/type-aware-scheduler
+kind load docker-image type-aware-scheduler
