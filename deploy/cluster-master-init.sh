8508846de798ac8579895820466f6c305a5c24f6
pawel <pawelzell@gmail.com>
2020-03-17 15:04:34 +0100

    Fix cluster network

--- deploy/cluster-master-init.sh
+++ deploy/cluster-master-init.sh
@@ -4,4 +4,5 @@ sudo kubeadm init --pod-network-cidr=10.244.0.0/16 --apiserver-advertise-address
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
-kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
+kubectl apply -f flannel.yaml
+#kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
