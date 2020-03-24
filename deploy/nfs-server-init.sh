f7405c0ac2dee02140b07e7a887a15ae76257baf
pawel <pawelzell@gmail.com>
2020-03-24 10:27:55 +0100

    nfs scripts update, allow pod scheduling on control plane

--- deploy/nfs-server-init.sh
+++ deploy/nfs-server-init.sh
@@ -1,5 +1,6 @@
 DIR="/mnt/k8s-type-aware-scheduler"
 INFLUXDB_DIR="${DIR}/influxdb"
+SERVER_IP="10.9.99.2"
 CLIENT_IP="10.9.99.1"
 
 sudo apt update
@@ -10,6 +11,7 @@ sudo mkdir -p ${INFLUXDB_DIR}
 sudo chown nobody:nogroup ${INFLUXDB_DIR}
 sudo chmod 777 ${DIR}
 echo "${DIR} ${CLIENT_IP}(rw,sync,no_subtree_check)" | sudo tee -a /etc/exports
+echo "${DIR} ${SERVER_IP}(rw,sync,no_subtree_check)" | sudo tee -a /etc/exports
 sudo exportfs -a
 sudo systemctl restart nfs-kernel-server
 #sudo ufw allow from ${CLIENT_IP} to any port nfs
