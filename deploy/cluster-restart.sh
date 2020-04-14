1dc84c984d08a52751989eee0bd8b9e26717b83b
pawel <pawelzell@gmail.com>
2020-04-14 11:44:49 +0200

    ./cluster-restart.sh

---
+++ deploy/cluster-restart.sh
@@ -0,0 +1,7 @@
+kMETRICS_SERVER=../../metrics-server
+./cluster-reset.sh
+./cluster-master-init.sh && 
+  ./up.sh && 
+  kubectl apply -f $kMETRICS_SERVER/deploy/1.8+/ && 
+  kubectl apply -f ../core-scheduler/deployment.yaml
+
