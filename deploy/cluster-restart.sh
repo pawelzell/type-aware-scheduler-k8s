0f53468a68a803f43bebd3c4372dcdaa67d272c3
pawel <pawelzell@gmail.com>
2020-06-04 08:38:34 +0200

    Script update with round-robin scheduler

--- deploy/cluster-restart.sh
+++ deploy/cluster-restart.sh
@@ -4,8 +4,9 @@ kMETRICS_SERVER=../../metrics-server
 ./cluster-master-init.sh && 
   ./up.sh && 
   kubectl apply -f $kMETRICS_SERVER/deploy/1.8+/ && 
-  kubectl apply -f ../main/deployment.yaml #&&
-  kubectl apply -f ../main/deployment-random.yaml #&&
-  kubectl apply -f ../metrics-collector/deployment.yaml #&&
+  kubectl apply -f ../metrics-collector/deployment.yaml &&
+  kubectl apply -f ../main/deployment.yaml &&
+  kubectl apply -f ../main/deployment-random.yaml &&
+  kubectl apply -f ../main/deployment-round-robin.yaml
   # ./up-os-metrics-collector.sh
 
